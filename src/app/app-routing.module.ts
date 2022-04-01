c07948ef8864bc389eb7ed2c9912c39f0e998432
Rajat <rajatranjan416@gmail.com>
2022-04-01 11:04:41 +0530

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LinksComponent } from './pages/links/links.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -10,7 +9,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'link', component: LinksComponent },
 ];
 
 @NgModule({
@@ -19,4 +17,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule {}
 
-export const routingComponents = [LoginComponent, HomeComponent, RegisterComponent, LinksComponent]
+export const routingComponents = [LoginComponent, HomeComponent, RegisterComponent]

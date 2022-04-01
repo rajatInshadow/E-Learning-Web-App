3f15bbeb4a9ea0394f7191d665d85ed1f00e55da
Rajat <rajatranjan416@gmail.com>
2022-04-01 21:52:43 +0530

    just commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterState } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'institute';
+  public displayNavbar = false;
 
 
-constructor(){
+constructor(private routeService:RouterState){
 
 }
+
+public route
+
 
 onClick(){
    

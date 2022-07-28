700cba2f24e757e69511c7b32237e218d3cfc679
Rajat <rajatranjan416@gmail.com>
2022-07-28 20:37:38 +0530

    commit

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,31 +12,40 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  public flag:boolean=false;
+
+
+  public flag: boolean = false;
   registerForm!: FormGroup; //Give the same Formgroup name as in template
 
   constructor(private formBuilder: FormBuilder) {
-    this.registerForm = this.formBuilder.group({ //Building the form using FormBuilder
-      firstName: new FormControl(),       //In the FormBuilder - we are creating a group of form elements
-      lastName: new FormControl(),        //these should be exactly same as form
-      emailAddress: ['',Validators.required],  
-      phoneNo: new FormControl(),
+
+    
+
+    this.registerForm = this.formBuilder.group({
+      //Building the form using FormBuilder
+      firstName: new FormControl(), //In the FormBuilder - we are creating a group of form elements
+      lastName: new FormControl(), //these should be exactly same as form
+      emailAddress: ['', [Validators.required, Validators.email]],// validatiors for email
+      phoneNo: [
+        '',
+        [
+          Validators.required,             //validators for mobile number
+          Validators.minLength(10),
+          Validators.maxLength(10),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
       password: new FormControl(),
-      confirmPassword:new FormControl(),
+      confirmPassword: new FormControl(),
       Address: new FormControl(),
-      pincode: new FormControl()
+      pincode: new FormControl(),
     });
-
-     
   }
 
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 
-  postData(){
+  postData() {
     console.log(this.registerForm);
-        console.log(this.registerForm.value);
+    console.log(this.registerForm.value);
   }
-  
 }
